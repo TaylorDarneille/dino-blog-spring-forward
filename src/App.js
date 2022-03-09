d31373526137c5db378b75faf2c39bb5738a19be
weston-bailey <wdbailey87@gmail.com>
2022-03-09 12:20:38 -0800

    mvp

--- src/App.js
+++ src/App.js
@@ -4,6 +4,38 @@ import Post from './Post'
 import posts from './posts'
 
 class App extends Component {
+  // 'controlled form' state
+  state = {
+    title: '',
+    author: '',
+    body: '',
+    comments: []
+  }
+
+  handleSubmit = e => {
+    // prevent the defualt behavoir of the synthetic form submission even
+    e.preventDefault()
+    console.log(e)
+    // add whatever is in state to the array of posts
+    posts.push(this.state) // imiatation axios
+    // reset state to trigger a re-render
+    this.setState({
+      title: '',
+      author: '',
+      body: '',
+      comments: []
+    })
+  }
+
+  handleTextChange = (e) => {
+    // input change synthetic event
+    //  dp npt need the prev value in a controlled form
+    // triggers re-render on every key press
+    this.setState({
+      body: e.target.value
+    }, () => console.log(this.state))
+  }
+
   render() {
     const postComponents = posts.map((post, index) => {
       return (
@@ -19,6 +51,22 @@ class App extends Component {
     return(
       <>
         {postComponents}
+
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor='body'>Post:</label>
+          <textarea
+            id="body"
+            // need some stuff for the controlled -- 
+            // 1. add ad event handler for when the user types into the form (onChange handler) that updates state
+            onChange={this.handleTextChange}
+            // 2. set the value of the input to be what is in state
+            value={this.state.body}
+
+          >
+          </textarea>
+
+          <input type="submit" />
+        </form>
       </>
     )
   }
